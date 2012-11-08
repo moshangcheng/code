07e2f0f9362ed09af0e4eb85d1b9e9437c850037
Sheng HU <moshangcheng@gmail.com>
2012-11-08 19:00:54 +0800

    test scanf
    test fmt.scanf

--- Go/HelloGo/src/test.go
+++ Go/HelloGo/src/test.go
@@ -1,13 +1,18 @@
 package main
  
 import (
-	"eulerproject"
-	"gcj/gcj2012"
-	"gcj/gcj2011"
+	//"eulerproject"
+	//"gcj/gcj2012"
+	//"gcj/gcj2011"
+	"fmt"
 )
  
 func main() {
-	eulerproject.Run()
-	gcj2011.Run()
-	gcj2012.Run()
+	//eulerproject.Run()
+	//gcj2011.Run()
+	//gcj2012.Run()
+	var intNumber int
+	var floatNumber float64
+	fmt.Println(fmt.Scanf("%d%f", &intNumber, &floatNumber))
+	fmt.Println(intNumber, floatNumber)
 }
