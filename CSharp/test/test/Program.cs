12d73fa42597c44f4d384c3193e5dbfc38066017
shu <moshangcheng@gmail.com>
2013-04-13 02:02:32 +0800

    add local code

---
+++ CSharp/test/test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Out.Write("hello");
+        }
+    }
+}
