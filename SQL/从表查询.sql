2bf8f194f0f0916f6dc1a3e9379ed6e8b0e7c314
moshangcheng <moshangcheng@gmail.com>
2013-09-27 23:48:09 +0800

    renmae some file

--- SQL/从表查询.sql
+++
@@ -1,2 +0,0 @@
-select * from user_constraints where 
-R_CONSTRAINT_NAME in (select constraint_name from user_constraints where table_name = 'CRM_DEPT') 
--- SQL/循环语句.sql
+++
@@ -1,6 +0,0 @@
-declare 
-  begin 
-  for i in 1 .. 20  loop 
-  Insert into CRM_COMPARE (COM_ID,COM_CPT_ID, COM_FIRST_PRICE, COM_SECOND_PRICE,COM_PROJECT_ID) values(SEQUENCE_COMPARE.nextval , 70, sys.dbms_random.value(100, 500)*1000 , sys.dbms_random.value(100, 500)*1000, 362); 
-  end loop;
- end;
